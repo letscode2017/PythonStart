1a3b7569b6c3e69bb732b27016bca284ca35ef8e
tgcodingplace <153830992+tgcodingplace@users.noreply.github.com>
2024-06-30 01:06:47 +0000

    Eine kleine Anrede

---
+++ 01Basics/SayMyName.py
@@ -0,0 +1,11 @@
+vorname = "Eddie"
+nachname = "Lowpenny"
+
+print("Hallo" + " " + vorname + " " + nachname)
+
+# oder so geht es auch
+
+anrede = "Hallo" + " " + vorname + " " + nachname
+
+print(anrede)
+
